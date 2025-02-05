6d58c737943ac7dca6377c2e9102efabb4983a65
Siddhcreator-1706 <shahsiddhb@gmail.com>
2025-02-05 18:25:01 +0530

    Updated code
    File updated to add printboard()

--- Snake_Game.cpp
+++ Snake_Game.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <iostream>
+#include <iomanip>
 #include <unistd.h>
 #ifdef _WIN32
 #include <windows.h>
@@ -9,9 +11,12 @@
 #include <stdbool.h>
 #include <sys/ioctl.h>
 #include <termios.h>
+#include <termios.h>   // setup terminal features
 #include <fcntl.h>
+#define Sleep usleep
 
-int kbhit() {
+int kbhit()
+{
     struct termios oldt, newt;
     int ch;
     int oldf;
@@ -28,7 +33,8 @@ int kbhit() {
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-    if (ch != EOF) {
+    if (ch != EOF)
+    {
         ungetc(ch, stdin);
         return 1;
     }
@@ -36,7 +42,8 @@ int kbhit() {
     return 0;
 }
 
-char getmychar(){
+char getmychar()
+{
     struct termios oldt, newt;
     char ch;
     int oldf;
@@ -52,28 +59,158 @@ char getmychar(){
 
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     fcntl(STDIN_FILENO, F_SETFL, oldf);
-    
+
     return ch;
 }
+
 #endif
 
 using namespace std;
 
-int main() {
+bool gameRunning = true;
+void getTerminalSize(int &width, int &height)
+{
+#ifdef _WIN32
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+    {
+        width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    }
+    else
+    {
+        width = 80;
+        height = 25;
+    }
+#else
+    struct winsize w;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1 || w.ws_col == 0) {
+        width = 80;  
+        height = 25; 
+    } else {
+        width = w.ws_col;
+        height = w.ws_row;
+}
 
-    char ch;
-    printf("Press any key to continue...\n");
-    while (!kbhit()) {
-        // Do something while waiting for key press
-        printf("HI...");
-        ch=getmychar();
-        if(getmychar()=='W'){
-            while(!kbhit()){
-                printf("seed...");
-            }
+#endif
+}
+
+class Snake
+{
+private:
+    vector<pair<int, int>> v;
+    int length;
+
+public:
+    Snake()
+    {
+        v.push_back({0, 0});
+        length = 1;
+    }
+    int getLength()
+    {
+        return length;
+    }
+    void moveSnake() {}
+    void changeLength()
+    {
+        length++;
+    }
+    void printSnake() {}
+};
+
+int setDifficulty()
+{
+    int difficulty;
+    cout << "Enter number to adjust difficulty level: " << endl;
+    cout << "1. Easy" << endl;
+    cout << "2. Medium" << endl;
+    cout << "3. Hard" << endl;
+    cout << "Difficulty level: ";
+    cin >> difficulty;
+    switch (difficulty)
+    {
+    case 3:
+        return 100;
+        break;
+    case 2:
+        return 250;
+        break;
+    case 1:
+        return 500;
+        break;
+    default:
+        return 225;
+        break;
+    }
+}
+
+void setColor(int color)
+{
+    cout << "\033[" << color << "m";
+}
+
+void resetColor()
+{
+    cout << "\033[0m";
+}
+
+void printBoard()
+{
+    int width, height;
+    char c=(char)(177);
+    getTerminalSize(width, height);
+    width = 3 * min(height, width) / 4;
+    height = width;
+    setColor(33);
+    for (int j = 0; j < height; j++)
+    {
+        cout << c << c;
+        for (int i = 0; i < width - 2; i++)
+        {
+            if (j == 0 || j == height - 1)
+                cout << c << c;
+            else
+                cout << setw(2) << " ";
+        }
+        cout << c << c << endl;
+    }
+    resetColor();
+}
+
+void userInput()
+{
+    if (kbhit())
+    {
+        switch (tolower(getmychar()))
+        {
+        case 'w':
+            // Move up
+            break;
+        case 's':
+            // Move down
+            break;
+        case 'a':
+            // Move left
+            break;
+        case 'd':
+            // Move right
+            break;
+        case 'n':
+            gameRunning = false;
+            break;
         }
     }
-    
-    printf("Key Pressed!\n");
+}
+
+int main()
+{
+    int dfc = setDifficulty();
+    printBoard();
+    // while (gameRunning)
+    // {
+    //     userInput();
+    //     Sleep(dfc);
+    // }
     return 0;
 }
